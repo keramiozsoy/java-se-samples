0c06db413b7d9cb213ccc6577f9949b6c482a8d3
Kerami Ozsoy <keramiozsoy@gmail.com>
2017-10-02 22:59:02 +0300

    Added types of comment

--- HelloWorldApp/src/com/linux/kerami/Test.java
+++ HelloWorldApp/src/com/linux/kerami/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java World !!");
+
+		/* comment_1  */
+		/** comment_2 */
+		// comment_3
 	}
 
 }
